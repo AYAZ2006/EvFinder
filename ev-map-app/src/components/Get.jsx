3c64abbed71e42ab7e78cfecf2a37c11d8a6acfb
Mohammed Ayaz <hajrasultana7075@gmail.com>
2025-07-20 22:45:04 +0530

    Update

--- ev-map-app/src/components/Get.jsx
+++ ev-map-app/src/components/Get.jsx
@@ -1,14 +1,16 @@
-import React,{useEffect,useState} from "react"
-import {MapContainer,TileLayer,Marker,Popup} from "react-leaflet"
+import React,{useEffect,useState,useRef} from "react"
+import {MapContainer,TileLayer,Marker,Popup,Polyline,useMap} from "react-leaflet"
 import axios from "axios"
-import L from "leaflet"
+import L, { polyline } from "leaflet"
 import "leaflet/dist/leaflet.css"
-
 const userIcon=new L.Icon({iconUrl:"https://cdn-icons-png.flaticon.com/128/149/149071.png",iconSize:[32,32],iconAnchor:[16,32],popupAnchor:[0,-32]})
 const stationIcon=new L.Icon({iconUrl:"https://cdn-icons-png.flaticon.com/128/684/684908.png",iconSize:[32,32],iconAnchor:[16,32],popupAnchor:[0,-32]})
 function Get(){
-  const[location,setLocation]=useState(null)
+  const[loc,setLocation]=useState(null)
   const[stations,setStations]=useState([])
+  const[route,setRoute]=useState(null)
+  const mapRef=useRef(null)
+  const[show,setShow]=useState(false)
   useEffect(()=>{
     navigator.geolocation.getCurrentPosition(
       async(pos)=>{
@@ -28,22 +30,63 @@ function Get(){
       }
     )
   },[])
-
+  const getRoute=async(lat,lon)=>{
+    if (!loc) return
+    try{
+      const res=await axios.get(`https://api.tomtom.com/routing/1/calculateRoute/${loc.lat},${loc.lon}:${lat},${lon}/json?key=Bi3W48sxr77pmKuHzk2DGVqNI7M9lyJP&traffic=true&instructionsType=tagged`)
+      const routeData=res.data.routes[0]
+      const points=routeData.legs[0].points.map((p)=>[p.latitude,p.longitude])
+      const instructions=routeData.guidance.instructions
+      setRoute({points,delay: routeData.summary.trafficDelayInSeconds,length: routeData.summary.lengthInMeters,travelTime: routeData.summary.travelTimeInSeconds,noTrafficTime: routeData.summary.noTrafficTravelTimeInSeconds,instructions})
+      setShow(true)
+    } catch (err) {
+      alert("Failed to fetch route")
+    }
+  }
+  function FitBoundsOnRoute({points}){
+    const map=useMap()
+    useEffect(()=>{
+      if(points&&points.length>0){
+        const bounds=L.latLngBounds(points)
+        map.fitBounds(bounds, {paddingTopLeft:[0,200],paddingBottomRight:[100,100],maxZoom:30})
+      }
+    },[points,map])
+    return null
+  }
   return(
-    <div style={{height:"100vh",width:"100vw"}}>
-      {location?(
-        <MapContainer center={[location.lat,location.lon]} zoom={13} style={{height:"100%",width:"100%"}}>
-          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'/>
-          <Marker position={[location.lat,location.lon]} icon={userIcon}><Popup>You are here</Popup></Marker>
-          {stations.map((station,idx)=>(
-            <Marker key={idx} position={[station.lat,station.lon]} icon={stationIcon}>
-              <Popup><strong>{station.name||"EV Charger"}</strong><br/>{station.address}</Popup>
-            </Marker>
-          ))}
-        </MapContainer>
+    <div ref={mapRef} style={{height:"100vh",width:"100vw"}} className="flex h-screen w-screen flex-col lg:flex-row">
+      {loc?(
+        <div className={`transition-all duration-300 ${show?"w-full lg:w-[80%]":"w-full"} h-[500vh] lg:h-full`}>
+          <MapContainer center={[loc.lat,loc.lon]} zoom={13} style={{height:"100%",width:"100%"}}>{route && <FitBoundsOnRoute points={route.points}/>}
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'/>
+            <Marker position={[loc.lat,loc.lon]} icon={userIcon}><Popup>You are here</Popup></Marker>
+            {stations.map((station,idx)=>(
+              <Marker key={idx} position={[station.lat,station.lon]} icon={stationIcon} eventHandlers={{click:()=>getRoute(station.lat,station.lon)}}>
+                <Popup><strong>{station.name||"EV Charger"}</strong><br/>{station.address}<br/><em>Click to route</em></Popup>
+              </Marker>
+            ))}
+            {route && <Polyline positions={route.points} color={route.delay>300?"red":route.delay>60?"orange":"green"} weight={4}></Polyline>}
+          </MapContainer>
+        </div>
       ):(
         <p style={{textAlign:"center",marginTop:"2rem"}}>Getting your location...</p>
       )}
+      {route && <>
+        <div className="lg:w-[20%] flex flex-col h-full p-6 text-white text-sm leading-relaxed shadow-lg overflow-y-auto">
+          <button onClick={()=>window.location.reload()}>Exit Navigation</button>
+          <div className="p-5 h-screen">
+            <strong className="text-lg">Route Summary</strong><br />
+            <p>Distance:{(route.length/1000).toFixed(1)} km<br/>Travel Time (w/traffic):{Math.round(route.travelTime/60)} min<br/>Travel Time (no traffic):{Math.round(route.noTrafficTime/60)} min<br/>Traffic Delay: {Math.round(route.delay / 60)} min</p>
+            <ul className="list-disc ml-4 py-2 space-y-2">
+              {route.instructions?.map((i, index) => {
+                const message=i.message.replace(/<street>(.*?)<\/street>/g, (_, name) => `<strong>${name}</strong>`)
+                return <li key={index} dangerouslySetInnerHTML={{__html:`In <strong>${Math.round((i.routeOffsetInMeters/1000)*100)/100} km</strong>,${message}`}}></li>
+              })}
+            </ul>
+          </div>
+        </div>
+      </>
+      }
     </div>
   )
 }
--- ev-map-app/src/components/Landing.jsx
+++ ev-map-app/src/components/Landing.jsx
@@ -8,7 +8,6 @@ import { useNavigate } from "react-router-dom"
 function Landing(){
   const {isSignedIn}=useAuth()
   const control=useAnimation()
-  const[ref,inView]=useInView({threshold:0.2})
   const infoSections=[
   {
     title: 'What is Our EV Charging Point?',
@@ -16,16 +15,28 @@ function Landing(){
   },
   {
     title: 'Why Choose Our Platform?',
-    paragraphs: ['We offer real-time station availability, route-based charger recommendations, and seamless payment options — all in one place.','Our app is designed for convenience, speed, and a user-friendly experience that puts you in control of your EV journey.','From data-driven station insights to 24/7 support, we’re focused on making EV charging effortless for everyone.Lets dive into the world of \electric vehicles']
+    paragraphs: ['We offer real-time station availability, route-based charger recommendations, and seamless payment options — all in one place.','Our app is designed for convenience, speed, and a user-friendly experience that puts you in control of your EV journey.','From data-driven station insights to 24/7 support, we’re focused on making EV charging effortless for everyone.Lets dive into the world of \electric vehicles and accessible solution for electric vehicle users.']
   },
   {
   title: 'Reliable Support, Anytime, Anywhere',
-  paragraphs: ["We're committed to providing 24/7 customer support for any charging-related concerns or questions.","Whether you're on a late-night road trip or exploring a new city, help is just a tap away through our app or hotline.","Our dedicated team monitors station performance and user feedback to ensure a smooth charging experience wherever you are."]
+  paragraphs: ["We're committed to providing 24/7 customer support for any charging-related concerns or questions.","Whether you're on a late-night road trip or exploring a new city, help is just a tap away through our app or hotline.","Our dedicated team monitors station performance and user feedback to ensure a smooth charging experience wherever you are.and accessible solution for electric vehicle users.','Whether you\'re commuting or on a road trip"]
   }
   ]
-  useEffect(()=>{
-    if(inView) control.start({opacity:1,y:0})
-  },[inView])
+const control1=useAnimation()
+const[ref1,inView1]=useInView({ threshold:0.2})
+const control2=useAnimation()
+const[ref2,inView2]=useInView({threshold:0.2})
+const control3=useAnimation()
+const[ref3,inView3]=useInView({threshold:0.2})
+useEffect(()=>{
+  if(inView1)control1.start({opacity:1,y:0})
+  },[inView1])
+useEffect(() => {
+  if(inView2)control2.start({opacity:1,y:0})
+  },[inView2])
+useEffect(()=>{
+  if(inView3)control3.start({opacity:1,y:0})
+  },[inView3])
   return(
     <div className="w-screen min-h-screen overflow-hidden bg-[url('/image.png')] bg-center bg-cover text-white">
       <nav className="backdrop-blur-3xl bg-white/10 border border-white/20 shadow-xl text-white px-10 py-4 rounded-2xl w-full max-w-xl mx-auto flex flex-wrap justify-between items-center gap-4 mt-15">
@@ -34,27 +45,47 @@ function Landing(){
             <button key={label}className="px-4 py-2 rounded-xl bg-white/20 border border-white/30 backdrop-blur-md hover:bg-white/30 transition shadow" onClick={()=>{if(label==='About')window.location.href='https://ev-route-finder.vercel.app/#/try';if (label==="Get Started"){if (isSignedIn) {window.location.href = "https://ev-route-finder.vercel.app/#/get";} else {window.location.href ="https://national-dane-76.accounts.dev/sign-in?redirect_url=https://ev-route-finder.vercel.app/#/get";}};if(label==='Reach Out')window.location.href='https://ev-route-finder.vercel.app/#/contact'}}>{label}</button>))}
         </div>
       </nav>
-      <Parallax pages={2} className="overflow-y-scroll scrollbar-hidden">
+      <Parallax pages={3} className="overflow-y-scroll scrollbar-hidden">
         <ParallaxLayer offset={0} speed={0} className="flex flex-col items-center gap-8 px-4 py-8">
-          {infoSections.slice(0,2).map((item,index)=>(
-            <motion.div key={index} ref={ref} initial={{ opacity: 100, y: 50 }} animate={control} transition={{ duration: 0.8, delay: index * 0.2 }} className="bg-gradient-to-r from-white via-gray-50 to-white shadow-2xl rounded-3xl w-full max-w-6xl h-fit transition-transform hover:scale-[1.02] hover:shadow-blue-100 flex items-center justify-center p-6 md:p-10">
-              <div className="flex flex-col md:flex-row items-start gap-4">
-                <div className="bg-blue-100 text-blue-600 rounded-full p-3">
-                  <svg className="w-6 h-6" fill="none" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" d="M13 16h-1v-4h-1m1-4h.01M12 20a8 8 0 100-16 8 8 0 000 16z"/></svg>
-                </div>
-                <div>
-                  <h2 className="text-2xl sm:text-3xl font-bold text-blue-800 mb-2">{item.title}</h2>
-                  {item.paragraphs.map((text, i) => (
-                    <p key={i} className={`text-gray-700 leading-relaxed ${i !== 0 ? 'mt-4' : ''}`}>{text}</p>
-                  ))}
-                </div>
+          <motion.div ref={ref1} initial={{ opacity: 100, y: 50 }} animate={control1} transition={{ duration: 0.8, delay: 0.2 }} className="bg-gradient-to-r from-white via-gray-50 to-white shadow-2xl rounded-3xl w-full max-w-6xl h-fit transition-transform hover:scale-[1.02] hover:shadow-blue-100 flex items-center justify-center p-6 md:p-10">
+            <div className="flex flex-col md:flex-row items-start gap-6">
+              <img src="/bike.png" className="max-h-90 w-auto object-contain rounded-xl md:mr-6"></img>
+              <div className="bg-blue-100 text-blue-600 rounded-full p-3 self-start">
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M13 16h-1v-4h-1m1-4h.01M12 20a8 8 0 100-16 8 8 0 000 16z"/>
+                </svg>
               </div>
-            </motion.div>
-          ))}
+              <div>
+                <h2 className="text-2xl sm:text-3xl font-bold text-blue-800 mb-2">{infoSections[0].title}</h2>
+                {infoSections[0].paragraphs.map((text, i) => (
+                  <p key={i} className={`text-gray-700 leading-relaxed ${i !== 0 ? 'mt-4' : ''}`}>{text}</p>
+                ))}
+              </div>
+            </div>
+          </motion.div>
         </ParallaxLayer>
         <ParallaxLayer offset={1} speed={0} className="flex flex-col items-center gap-8 px-4 py-8">
-          <motion.div ref={ref} initial={{ opacity: 0, y: 50 }} animate={control} transition={{ duration: 0.8, delay: 0.2 }} className="bg-gradient-to-r from-white via-gray-50 to-white shadow-2xl rounded-3xl w-full max-w-6xl h-fit transition-transform hover:scale-[1.02] hover:shadow-blue-100 flex items-center justify-center p-6 md:p-10">
+          <motion.div ref={ref2} initial={{ opacity: 0, y: 50 }} animate={control2} transition={{ duration: 0.8, delay: 0.1 }} className="bg-gradient-to-r from-white via-gray-50 to-white shadow-2xl rounded-3xl w-full max-w-6xl h-fit transition-transform hover:scale-[1.02] hover:shadow-blue-100 flex items-center justify-center p-6 md:p-10">
+            <div className="flex flex-col md:flex-row items-start gap-6">
+              <img src="/car.png" className="max-h-80 w-auto object-contain rounded-xl md:mr-6 lg:mr-9"/>
+              <div className="bg-blue-100 text-blue-600 rounded-full p-3 self-start">
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M13 16h-1v-4h-1m1-4h.01M12 20a8 8 0 100-16 8 8 0 000 16z"/>
+                </svg>
+              </div>
+              <div>
+                <h2 className="text-2xl sm:text-3xl font-bold text-blue-800 mb-2">{infoSections[1].title}</h2>
+                {infoSections[1].paragraphs.map((text, i) => (
+                  <p key={i} className={`text-gray-700 leading-relaxed ${i !== 0 ? 'mt-4' : ''}`}>{text}</p>
+                ))}
+              </div>
+            </div>
+          </motion.div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={2} speed={0} className="flex flex-col items-center gap-8 px-4 py-8">
+          <motion.div ref={ref3} initial={{ opacity: 0, y: 50 }} animate={control3} transition={{ duration: 0.8, delay: 0.2 }} className="bg-gradient-to-r from-white via-gray-50 to-white shadow-2xl rounded-3xl w-full max-w-6xl h-fit transition-transform hover:scale-[1.02] hover:shadow-blue-100 flex items-center justify-center p-6 md:p-10">
             <div className="flex flex-col md:flex-row items-start gap-4">
+              <img src="/auto1.png" className="max-h-80 w-auto object-contain rounded-xl md:mr-6 lg:mr-9 lg:mt-6"/>
               <div className="bg-blue-100 text-blue-600 rounded-full p-3">
                 <svg className="w-6 h-6" fill="none" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" d="M13 16h-1v-4h-1m1-4h.01M12 20a8 8 0 100-16 8 8 0 000 16z"/></svg>
               </div>
